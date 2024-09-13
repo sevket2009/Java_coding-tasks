c2655bb70dd25889afd6ea96bb681a7dde7a0c1f
ali.bilen <ali.bilen@outlook.com>
2024-09-13 13:22:55 +0200

    Odd and Even number.

---
+++ src/aliBilen/Task2.java
@@ -0,0 +1,16 @@
+package aliBilen;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 6;
+        if (5 % 2 == 1) {
+            System.out.println(num1 + " is Odd number");
+        }
+        if(6%2==0){
+            System.out.println(num2 + " is Even number");
+        }
+    }
+}
