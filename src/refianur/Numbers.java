18c200459f8d4abb521335e37b73000d7dedf8d0
9321ref <refianurcasa@gmail.com>
2024-09-13 11:07:13 +0200

    13/09 EvenOrOdd added 1

--- src/refianur/Numbers.java
+++ src/refianur/Numbers.java
@@ -3,14 +3,14 @@ package refianur;
 public class Numbers {
     public static void main(String[] args) {
 
-        EvenOrOdd(5);
-        EvenOrOdd(4);
+        identifier(5);
+        identifier(4);
 
 
     }
 
 
-    public static void EvenOrOdd(int number) {
+    public static void identifier(int number) {
         if( number % 2 == 0 ) {
             System.out.println( number + "  is a even.  " );
         }else {
