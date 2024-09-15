a4b402c74191fe45a6280fc9db99d1f6da600c20
Laurette <sonfacklaurette@gmail.com>
2024-09-15 10:34:44 +0200

    number modified

--- src/laurette/OddOrEven.java
+++ src/laurette/OddOrEven.java
@@ -3,8 +3,8 @@ package laurette;
 public class OddOrEven {
     public static void main(String[] args) {
 
-        System.out.println(identify(17));
-        System.out.println(identify(60));
+        System.out.println(identify(5));
+        System.out.println(identify(6));
 
     }
 
