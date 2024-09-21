d167d7e07ccab80e3730992666bd996847c99dca
kayanecati <kayanecati@icloud.com>
2024-09-21 11:49:18 +0200

    completed Task2 21/09/24

--- src/necati/Task2.java
+++ src/necati/Task2.java
@@ -1,4 +1,12 @@
 package necati;
 
 public class Task2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 30; i++) {
+            System.out.println(i % 15 == 0 ? "FINRA" : i % 3 == 0 ? "FIN" : i % 5 == 0 ? "RA" : i);
+        }
+
+    }
 }
