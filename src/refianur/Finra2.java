4c149c291725de6774186f660bebb48650aae64e
9321ref <refianurcasa@gmail.com>
2024-09-22 22:31:28 +0200

    22/09 ref4

---
+++ src/refianur/Finra2.java
@@ -0,0 +1,19 @@
+package refianur;
+
+public class Finra2 {
+    public static void main(String[] args) {
+
+
+        for(int i = 1; i <= 15; i++) {
+            if(i % 3 == 0 && i % 5 == 0 ) {
+                System.out.println("FINRA");
+            }else if(i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
