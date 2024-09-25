43c6480187b0d6aaf7d5785b5105440ca8e9eca2
nasderek <nadia_nasr@gmx.de>
2024-09-25 18:29:08 +0200

    second task finra completed updated

--- src/nadia/Task2Finra.java
+++ src/nadia/Task2Finra.java
@@ -2,12 +2,12 @@ package nadia;
 
 public class Task2Finra {
     public static void main(String[] args) {
-        for (int i=1;i<=30;i++) {
-            if (i%3==0 && i%5==0) {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FINRA");
-            } else if (i%3==0) {
+            } else if (i % 3 == 0) {
                 System.out.println("FIN");
-            } else if (i%5==0) {
+            } else if (i % 5 == 0) {
                 System.out.println("RA");
             } else {
                 System.out.println(i);
