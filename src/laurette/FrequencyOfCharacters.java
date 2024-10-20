5e6083380d6aaf1b354dcc5bb5b512f8654542e4
Laurette2024 <sonfacklaurette@gmail.com>
2024-10-20 21:28:39 +0200

    FrequencyOfCharacters method line added

--- src/laurette/FrequencyOfCharacters.java
+++ src/laurette/FrequencyOfCharacters.java
@@ -4,7 +4,7 @@ public class FrequencyOfCharacters {
 
     public static void main(String[] args) {
 
-         frequencyOfCharacters("AAABBCDD");
+         frequencyOfCharacters("AAABBCDD");//Example
 
     }
 
